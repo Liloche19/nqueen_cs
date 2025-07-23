072e82ff2edc868759b325c546a55885214c4335
Liloche19 <lilian.locher@epitech.eu>
2025-07-23 20:47:26 +0200

    [+] board initialization is working + getting the size of the board

--- Program.cs
+++ Program.cs
@@ -2,9 +2,35 @@ using System;
 
 class Program
 {
-    static void Main()
+    static int Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        return;
+        if (args.Length != 1)
+            return print_help(1);
+        uint size;
+        if (!uint.TryParse(args[0], out size))
+            return print_help(1);
+        int[,] board = init_board(size);
+        return 0;
+    }
+
+    static int print_help(int status)
+    {
+        Console.WriteLine("USAGE:");
+        Console.WriteLine($"\tdotnet run BOARD_SIZE");
+        Console.WriteLine();
+        Console.WriteLine("DESCRIPTION:");
+        Console.WriteLine("\tBOARD_SIZE\tis the size of the board, and also the number of queen to place");
+        return status;
+    }
+
+    static int[,] init_board(uint size)
+    {
+        int[,] board;
+
+        board = new int[size, size];
+        for (int i = 0; i < size; i++)
+            for (int j = 0; j < size; j++)
+                board[i, j] = 0;
+        return board;
     }
 }
