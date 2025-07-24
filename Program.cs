7a461cd4d06824cf9d77df5ecf6c0f8dd0284f21
Liloche19 <lilian.locher@epitech.eu>
2025-07-24 12:47:53 +0200

    [+] the program is working

--- Program.cs
+++ Program.cs
@@ -9,7 +9,9 @@ class Program
         uint size;
         if (!uint.TryParse(args[0], out size))
             return print_help(1);
-        int[,] board = init_board(size);
+        bool[,] board = init_board(size);
+        int solutions = solve_nqueen(board, size, 0);
+        Console.WriteLine($"Number of solutions for a board of size {size}: {solutions}");
         return 0;
     }
 
@@ -23,14 +25,46 @@ class Program
         return status;
     }
 
-    static int[,] init_board(uint size)
+    static bool[,] init_board(uint size)
     {
-        int[,] board;
+        bool[,] board;
 
-        board = new int[size, size];
+        board = new bool[size, size];
         for (int i = 0; i < size; i++)
             for (int j = 0; j < size; j++)
-                board[i, j] = 0;
+                board[i, j] = false;
         return board;
+    }
+
+    static int solve_nqueen(bool[,] board, uint size, int actual_row)
+    {
+        if (actual_row == size)
+            return 1;
+        int solutions = 0;
+        for (int i = 0; i < size; i++)
+        {
+            if (is_safe(board, size, actual_row, i))
+            {
+                board[actual_row, i] = true;
+                solutions += solve_nqueen(board, size, actual_row + 1);
+                board[actual_row, i] = false;
+            }
+        }
+        return solutions;
+    }
+
+    static bool is_safe(bool[,] board, uint size, int x, int y)
+    {
+        for (int i = 0; i < size; i++)
+            if (x - i >= 0 && y - i >= 0 && board[x - i, y - i] ||
+                x + i < size && y - i >= 0 && board[x + i, y - i] ||
+                x + i < size && y + i < size && board[x + i, y + i] ||
+                x - i >= 0 && y + i < size && board[x - i, y + i] ||
+                x - i >= 0 && board[x - i, y] ||
+                x + i < size && board[x + i, y] ||
+                y - i >= 0 && board[x, y - i] ||
+                y + i < size && board[x, y + i])
+                return false;
+        return true;
     }
 }
